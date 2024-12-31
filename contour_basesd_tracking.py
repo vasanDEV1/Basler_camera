7b815ef71323c5c3911103091f8f3d0c6489b485
vasanDEV <krvasan2003@gmail.com>
2024-12-31 10:00:26 +0530

    new commit

--- contour_basesd_tracking.py
+++ contour_basesd_tracking.py
@@ -6,54 +6,52 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 def main():
-    camera = py.InstantCamera(py.TlFactory.GetInstance().CreateFirstDevice())   
+    # Initialize the camera
+    camera = py.InstantCamera(py.TlFactory.GetInstance().CreateFirstDevice())
     camera.Open()
     camera.PixelFormat.SetValue("BayerRG8")
     camera.ExposureAuto.SetValue("Continuous")
     camera.GainAuto.SetValue("Continuous")
     camera.AcquisitionMode.SetValue("Continuous")
     camera.StartGrabbing(py.GrabStrategy_LatestImageOnly)
-    
+
     try:
         while camera.IsGrabbing():
             grab_result = camera.RetrieveResult(2000, py.TimeoutHandling_ThrowException)
             if grab_result.GrabSucceeded():
+                
                 img = grab_result.GetArray()
 
-                _, thresh2 = cv2.threshold(img, 180, 255, cv2.THRESH_BINARY)
-                contours,hierarchy = cv2.findContours(thresh2,cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+                
+                _, thresh2 = cv2.threshold(img, 150, 255, cv2.THRESH_BINARY)
 
                 
-                for index in range(len(contours)):
-                
-                    cnt=contours[index]
-                
-                    x,y,w,h = cv2.boundingRect(cnt)
-                
-                    cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),2)
+                contours, hierarchy = cv2.findContours(thresh2, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
 
                 
-                plt.imshow(img)
-
-
-
-
-
+                for contour in contours:
+                    x, y, w, h = cv2.boundingRect(contour)
+                    cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
                 
+                #cv2.imshow('Thresholded Image', thresh2)
                 cv2.namedWindow('Image',cv2.WINDOW_NORMAL)
-                #cv2.resizeWindow('Image', 500,500)
-                cv2.imshow('Image', thresh2)
-                if cv2.waitKey(1) & 0xFF == ord('q'):  
+                cv2.imshow('Image', img)
+
+                
+                if cv2.waitKey(1) & 0xFF == ord('q'):
                     break
+
             grab_result.Release()  
+
     except Exception as e:
         print(f"An error has occurred: {e}")
+
     finally:
+        
         camera.StopGrabbing()
         camera.Close()
-        cv2.destroyAllWindows() 
+        cv2.destroyAllWindows()
 
 if __name__ == "__main__":
     main()
-
